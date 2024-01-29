dfd9c0bb042ff8615449637923caa8a6c6e317e4
abby baldinger <aebaldinger@stu.naperville203.org>
2024-01-29 10:29:50 -0600

    worked on disable and logout and routing

--- assets/js/barista.js
+++ assets/js/barista.js
@@ -3,31 +3,37 @@
  *  (primarily event handlers to fetch data from the Node server)
  */
 
-const submitButton = document.querySelector("input.submit");
-submitButton.addEventListener("click", async () => {
-  // get the values entered by the user
-  const date = document.querySelector("input.date").value;
+// const submitButton = document.querySelector("input.submit");
+// submitButton.addEventListener("click", async () => {
+//   // get the values entered by the user
+//   const date = document.querySelector("input.date").value;
 
-  // a more sophisticated select that selects all input elements of class
-  //  habits that are checked
-  const habitOfMindButtons = document.querySelectorAll("input.habits:checked");
-  const habitOfMind =
-    habitOfMindButtons.length > 0 ? habitOfMindButtons[0].value : null;
+//   // a more sophisticated select that selects all input elements of class
+//   //  habits that are checked
+//   const habitOfMindButtons = document.querySelectorAll("input.habits:checked");
+//   const habitOfMind =
+//     habitOfMindButtons.length > 0 ? habitOfMindButtons[0].value : null;
 
-  const content = document.querySelector("textarea.content").value;
-  const entry = { date, habit: habitOfMind, content };
+//   const content = document.querySelector("textarea.content").value;
+//   const entry = { date, habit: habitOfMind, content };
 
-  const response = await fetch("/barista", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+//   const response = await fetch("/barista", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error creating entry");
-  }
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error creating entry");
+//   }
+// });
+
+const disableEnableButton = document.querySelector("dis-en");
+
+disableEnableButton.addEventListener("click", function () {
+  changeText.textContent = "Text has been changed!";
 });
