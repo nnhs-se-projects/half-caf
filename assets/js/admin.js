4b00dcda41d8eea261d5fba8ea72020870ad2406
tkgadhoke <tkgadhoke@stu.naperville203.org>
2024-01-29 01:49:36 -0600

    finished add user layout (homepage)

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -37,113 +37,113 @@ deleteUserButton.addEventListener("click", async () => {
   }
 });
 
-const viewUserButton = document.querySelector("input.submit");
-viewUserButton.addEventListener("click", async () => {
-  const response = await fetch("/viewUser", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const viewUserButton = document.querySelector("input.submit");
+// viewUserButton.addEventListener("click", async () => {
+//   const response = await fetch("/viewUser", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error viewing user");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error viewing user");
+//   }
+// });
 
-const addDrinkButton = document.querySelector("input.submit");
-addDrinkButton.addEventListener("click", async () => {
-  const response = await fetch("/addDrink", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const addDrinkButton = document.querySelector("input.submit");
+// addDrinkButton.addEventListener("click", async () => {
+//   const response = await fetch("/addDrink", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error adding drink");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error adding drink");
+//   }
+// });
 
-const modifyDrinkButton = document.querySelector("input.submit");
-modifyDrinkButton.addEventListener("click", async () => {
-  const response = await fetch("/modifyDrink", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const modifyDrinkButton = document.querySelector("input.submit");
+// modifyDrinkButton.addEventListener("click", async () => {
+//   const response = await fetch("/modifyDrink", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error modifying drink");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error modifying drink");
+//   }
+// });
 
-const deleteDrinkButton = document.querySelector("input.submit");
-modifyDrinkButton.addEventListener("click", async () => {
-  const response = await fetch("/deleteDrink", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const deleteDrinkButton = document.querySelector("input.submit");
+// modifyDrinkButton.addEventListener("click", async () => {
+//   const response = await fetch("/deleteDrink", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error modifying drink");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error modifying drink");
+//   }
+// });
 
-const addFlavorButton = document.querySelector("input.submit");
-addFlavorButton.addEventListener("click", async () => {
-  const response = await fetch("/addFlavor", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const addFlavorButton = document.querySelector("input.submit");
+// addFlavorButton.addEventListener("click", async () => {
+//   const response = await fetch("/addFlavor", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error modifying drink");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error modifying drink");
+//   }
+// });
 
-const deleteFlavorButton = document.querySelector("input.submit");
-deleteFlavorButton.addEventListener("click", async () => {
-  const response = await fetch("/deleteFlavor", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(entry),
-  });
+// const deleteFlavorButton = document.querySelector("input.submit");
+// deleteFlavorButton.addEventListener("click", async () => {
+//   const response = await fetch("/deleteFlavor", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(entry),
+//   });
 
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error modifying drink");
-  }
-});
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error modifying drink");
+//   }
+// });
 
-const logoutButton = document.querySelector("input.submit");
-logoutButton.addEventListener("click", async () => {
-  if (response.ok) {
-    window.location = "/";
-  } else {
-    console.log("error modifying drink");
-  }
-});
+// const logoutButton = document.querySelector("input.submit");
+// logoutButton.addEventListener("click", async () => {
+//   if (response.ok) {
+//     window.location = "/";
+//   } else {
+//     console.log("error modifying drink");
+//   }
+// });
--- server/routes/router.js
+++ server/routes/router.js
@@ -4,12 +4,8 @@ const Entry = require("../model/entry");
 
 route.get("/", async (req, res) => {});
 
-route.get("/admin", (req, res) => {
-  res.render("admin", {
-    // entries: formattedEntries,
-    // habits: habitsOfMind,
-    // selectedHabit: habit,
-  });
+route.get("/addUser", (req, res) => {
+  res.render("addUser", {});
 });
 
 // delegate all authentication to the auth.js router
