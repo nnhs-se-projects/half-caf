b470c986848cdcbdd486d792816e768edb67c406
tkgadhoke <tkgadhoke@stu.naperville203.org>
2024-02-05 10:27:25 -0600

    working on google oauth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,31 +1,31 @@
-// /**
-//  * Routes for authentication using the Google Sign-In API
-//  */
+/**
+ * Routes for authentication using the Google Sign-In API
+ */
 
-// // cSpell:ignoreRegExp /[^\s]{40,}/
+// cSpell:ignoreRegExp /[^\s]{40,}/
 
-// const express = require("express");
-// const route = express.Router();
+const express = require("express");
+const route = express.Router();
 
-// const { OAuth2Client } = require("google-auth-library");
+const { OAuth2Client } = require("google-auth-library");
 
-// const CLIENT_ID =
-//   "1022838194773-p8g5ac0qr11mfko61qurgnqdb9jitpjf.apps.googleusercontent.com";
-// const oAuth2 = new OAuth2Client(CLIENT_ID);
+const CLIENT_ID =
+  "1022838194773-p8g5ac0qr11mfko61qurgnqdb9jitpjf.apps.googleusercontent.com";
+const oAuth2 = new OAuth2Client(CLIENT_ID);
 
-// route.post("/", async (req, res) => {
-//   const token = req.body.token;
-//   console.log("Token..." + token);
-//   const ticket = await oAuth2.verifyIdToken({
-//     idToken: token,
-//     audience: CLIENT_ID,
-//   });
+route.post("/", async (req, res) => {
+  const token = req.body.token;
+  console.log("Token..." + token);
+  const ticket = await oAuth2.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,
+  });
 
-//   // store the authenticated user's email in the session
-//   const { sub, email } = ticket.getPayload();
-//   req.session.email = email;
-//   console.log(sub, email);
-//   res.status(201).end();
-// });
+  // store the authenticated user's email in the session
+  const { sub, email } = ticket.getPayload();
+  req.session.email = email;
+  console.log(sub, email);
+  res.status(201).end();
+});
 
-// module.exports = route;
+module.exports = route;
