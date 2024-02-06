57c649f3543969cbed29bbda70dcf0cc8150ddf2
abby baldinger <aebaldinger@stu.naperville203.org>
2024-02-06 10:35:57 -0600

    fixed the popular drinks file but still cant get it to work

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 route.get("/", async (req, res) => {
-  res.render("auth");
+  res.render("homePopularDrinks");
 });
 
 route.get("/addUser", (req, res) => {
