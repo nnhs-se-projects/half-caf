86ade91d4663945faf9e38f7f80a9f82a71ae0e7
Eric Lai <emlai@stu.naperville203.org>
2024-02-21 11:04:31 -0600

    finished delete topping

---
+++ assets/js/deleteTopping.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+async function deleteTopping() {
+  const selectedId = document.querySelector("#toDelete").value;
+  console.log(selectedId);
+  const response = await fetch(`/deleteTopping/${selectedId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    window.location = "/deleteTopping";
+  } else {
+    console.log("error");
+  }
+}
