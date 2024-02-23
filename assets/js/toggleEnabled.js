e03df80f290911a4edc66c561cc837485138d205
dnmeerman <dnmeerman@stu.naperville203.org>
2024-02-23 10:29:58 -0600

    started the enable toggle js

---
+++ assets/js/toggleEnabled.js
@@ -0,0 +1,4 @@
+const toggleEnabled = document.querySelector("input.checkbox");
+console.log(toggleEnabled); // boolean value
+const id = "65d77b1087449294679afc91";
+toggleEnabled.addEventListener("click", async () => {});
