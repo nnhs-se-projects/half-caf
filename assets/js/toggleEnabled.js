84796cd598610399ff5b29b19a10bf82dda296bb
dnmeerman <dnmeerman@stu.naperville203.org>
2024-02-27 11:00:11 -0600

    worked on updating enabled

--- assets/js/toggleEnabled.js
+++ assets/js/toggleEnabled.js
@@ -1,4 +1,18 @@
+const Enabled = require("../model/user");
+
 const toggleEnabled = document.querySelector("#myCheckbox");
 console.log(toggleEnabled); // boolean value
 const id = "65d77b1087449294679afc91";
-toggleEnabled.addEventListener("click", async () => {});
+toggleEnabled.addEventListener("change", async () => {
+  const newValue = toggleEnabled.ariaChecked;
+  try {
+    const doc = await Enabled.FindByID(id);
+
+    doc.enabled = newValue;
+
+    await doc.save;
+    console.log(toggleEnabled); // boolean value
+  } catch (error) {
+    console.error("Error enabling website", error);
+  }
+});
