2c58875726fc2624b9ff65739b305c7a5aebcfd6
abby baldinger <aebaldinger@stu.naperville203.org>
2024-03-05 10:59:16 -0600

    got checkboxes to start working

--- assets/js/addDrink.js
+++ assets/js/addDrink.js
@@ -3,15 +3,21 @@ addDrinkButton.addEventListener("click", async () => {
   const name = document.getElementById("name").value;
   const description = document.getElementById("description").value;
   const price = document.getElementById("price").value;
-  const popular = document.getElementById("popular");
-  const temps = document.querySelectorAll("input.temps:checked");
-  const temp = temps.length > 0 ? temps[0].value : null;
-  const special = document.getElementById("special");
+  const popular = document.getElementById("popular").checked;
+  const temps = document.querySelectorAll("input.temps");
+  const checkedTemps = [];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i].checked) {
+      checkedTemps.push(temps[i].value);
+    }
+  }
+  console.log(checkedTemps);
+  const special = document.getElementById("special").checked;
   const flavors = document.querySelectorAll("input.flavors:checked");
   const flavor = flavors.length > 0 ? flavors[0].value : null;
   const toppings = document.querySelectorAll("input.toppings:checked");
   const topping = toppings.length > 0 ? toppings[0].value : null;
-  const caf = document.getElementById("caffeinated");
+  const caf = document.getElementById("caffeinated").checked;
 
   const menuItem = {
     name,
@@ -20,7 +26,7 @@ addDrinkButton.addEventListener("click", async () => {
     popular,
     flavor,
     topping,
-    temp,
+    temps,
     caf,
     special,
   };
