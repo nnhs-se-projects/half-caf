d21e7f5591d37d41fe61f5ab906c7509c951ed6e
dnmeerman <dnmeerman@stu.naperville203.org>
2024-03-05 10:58:05 -0600

    tested the toggle, event listener worksw but still won't connect to databse

--- assets/js/toggleEnabled.js
+++ assets/js/toggleEnabled.js
@@ -1,15 +1,28 @@
+const Enabled = require("./path/to/EnabledModel");
+
 const toggleEnabled = document.querySelector("input.toggle");
-console.log(toggleEnabled); // boolean value
+// console.log(toggleEnabled.checked); // boolean value
+
+let mongoObject = {
+  _id: "65d77b1087449294679afc91",
+  enabled: false,
+  __v: 0,
+};
 // const id = "65d77b1087449294679afc91";
 toggleEnabled.addEventListener("change", async () => {
   const newValue = toggleEnabled.checked;
   try {
+    let doc = await Enabled.findById(mongoObject._id);
     // const doc = await Enabled.FindByID("enabled");
 
     // doc.enabled = newValue;
 
     // await doc.save;
-    console.log(toggleEnabled); // boolean value
+    // console.log(toggleEnabled.checked); // boolean value
+
+    doc.enabled = newValue;
+    await doc.save();
+    console.log(doc.enabled);
   } catch (error) {
     console.error("Error enabling website", error);
   }
