243637de2ed757db4068e949378f2ff5ba68cc07
dnmeerman <dnmeerman@stu.naperville203.org>
2024-03-11 10:30:11 -0500

    enabl;e

--- assets/js/toggleEnabled.js
+++ assets/js/toggleEnabled.js
@@ -1,27 +1,9 @@
-const Enabled = require(".../enabled.js");
-
-const orderEnabledCheckbox = document.getElementById("input.toggle");
-
-orderEnabledCheckbox.addEventListener("change", function () {
-  const enabledValue = this.checked;
-  Enabled.findOneAndUpdate(
-    {},
-    { enabled: enabledValue },
-    { new: true, upsert: true },
-    function (err, doc) {
-      if (err) {
-        console.log("Error updating document: ", err);
-      } else {
-        console.log("Document updated: ", doc);
-      }
-    }
-  );
-
-  console.log("Enabled value:", enabledValue); // For demonstration purposes
-});
-/**
 const toggleEnabled = document.querySelector("input.toggle");
-// console.log(toggleEnabled.checked); // boolean value
+console.log(toggleEnabled.checked);
+toggleEnabled.addEventListener("change");
+/** 
+const toggleEnabled = document.querySelector("input.toggle");
+console.log(toggleEnabled.checked); // boolean value
 // const id = "65d77b1087449294679afc91";
 toggleEnabled.addEventListener("change", async () => {
   const newValue = toggleEnabled.checked;
@@ -31,11 +13,11 @@ toggleEnabled.addEventListener("change", async () => {
     // doc.enabled = newValue;
 
     // await doc.save;
-    // console.log(toggleEnabled.checked); // boolean value
+    console.log(newValue); // boolean value
 
     // console.log(doc.enabled);
   } catch (error) {
     console.error("Error enabling website", error);
   }
-}); 
+});
 */
