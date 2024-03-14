e3274c44e43b22fc346b13236bbeef4fd260306f
abby baldinger <aebaldinger@stu.naperville203.org>
2024-03-14 11:00:04 -0500

    tried getting activate/deactivate buttons to work

--- assets/js/viewUser.js
+++ assets/js/viewUser.js
@@ -5,7 +5,18 @@ userStatus.addEventListener("change", (event) => {
     // alert("All");
   } else if (userStatus.value === "Activated") {
     // alert("Activated");
-  } else if (userStatus.value === "Unactivated") {
-    // alert("Unactivated");
+  } else if (userStatus.value === "Deactivated") {
+    // alert("Deactivated");
   }
+});
+
+const activate = document.getElementById("activated");
+activate.addEventListener("click", async () => {
+  user.isActivated = true;
+  console.log("clicked");
+});
+const deactivate = document.getElementById("deactivated");
+deactivate.addEventListener("click", async () => {
+  user.isActivated = false;
+  console.log("clicked");
 });
