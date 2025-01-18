6ec16de75cb9382b3f2c534a74dfea636a19d444
Jesso4906 <bennettjesso4906@gmail.com>
2025-01-18 14:22:06 -0600

    add code to handle if there are no temperature options
    If a drink does not have any temperature options, the prompt to select a temp in the customize drink page will be hidden and the temp will be set as "Default".

--- assets/js/customizeDrink.js
+++ assets/js/customizeDrink.js
@@ -3,20 +3,22 @@ addToOrderButton.addEventListener("click", async () => {
   const name = document.getElementById("name").textContent;
   const price = document.getElementById("price").textContent.match(/(\d+)/)[0];
   const flavors = document.querySelectorAll("input#flavors");
-  let checkedFlavors = [];
+  const checkedFlavors = [];
   for (let i = 0; i < flavors.length; i++) {
     if (flavors[i].checked) {
       checkedFlavors.push(flavors[i].value);
     }
   }
   const toppings = document.querySelectorAll("input#toppings");
-  let checkedToppings = [];
+  const checkedToppings = [];
   for (let i = 0; i < toppings.length; i++) {
     if (toppings[i].checked) {
       checkedToppings.push(toppings[i].value);
     }
   }
-  const temp = document.querySelector("input.temps:checked").value;
+  const tempInput = document.querySelector("input.temps:checked");
+  const temp = tempInput !== null ? tempInput.value : "Default";
+
   const instructions = document.getElementById("instructions").value;
   const favorite = document.getElementById("favorite").checked;
 
