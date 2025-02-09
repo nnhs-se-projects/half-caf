235467748fc2435b02d7c017effa241b9711f67a
Jesso <43867752+Jesso4906@users.noreply.github.com>
2025-02-09 13:11:47 -0600

    Revert "Fix websocket issue"

--- assets/js/toggleEnabled.js
+++ assets/js/toggleEnabled.js
@@ -20,3 +20,23 @@ toggleEnabled.addEventListener("change", async () => {
     console.log("error");
   }
 });
+/** 
+const toggleEnabled = document.querySelector("input.toggle"); `
+console.log(toggleEnabled.checked); // boolean value
+// const id = "65d77b1087449294679afc91";
+toggleEnabled.addEventListener("change", async () => {
+  const newValue = toggleEnabled.checked;
+  try {
+    // const doc = await Enabled.FindByID("enabled");
+
+    // doc.enabled = newValue;
+
+    // await doc.save;
+    console.log(newValue); // boolean value
+
+    // console.log(doc.enabled);
+  } catch (error) {
+    console.error("Error enabling website", error);
+  }
+});
+*/
