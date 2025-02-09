4f7d3d86be7d1cd0dc89a98b0340e237ef470ad8
Jesso4906 <bennettjesso4906@gmail.com>
2025-02-09 17:58:47 -0600

    check if the socket server already exists before making a new one

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -2,28 +2,25 @@ const { Server } = require("socket.io");
 
 let io;
 function createSocketServer(httpServer) {
-  io = new Server(httpServer, {
-    connectionStateRecovery: {
-      // the backup duration of the sessions and the packets
-      maxDisconnectionDuration: 1000,
-      // whether to skip middlewares upon successful recovery
-      skipMiddlewares: true,
-    },
-    connectTimeout: 1000,
-  });
-
-  io.on("connection", (socket) => {
-    // console.log("a user connected");
-
-    // add additional socket.on code here to handle incoming events
-    //  from the client
-
-    socket.on("disconnect", () => {
-      // console.log("user disconnected");
+  if (!io) {
+    io = new Server(httpServer, {
+      connectionStateRecovery: {},
+      connectTimeout: 1000,
     });
 
-    return io;
-  });
+    io.on("connection", (socket) => {
+      // console.log("a user connected");
+
+      // add additional socket.on code here to handle incoming events
+      //  from the client
+
+      socket.on("disconnect", () => {
+        // console.log("user disconnected");
+      });
+    });
+  }
+
+  return io;
 }
 
 function emitToggleChange() {
