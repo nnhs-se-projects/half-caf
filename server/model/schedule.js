d82ca7d3f2121c05c173211c17f78739b11f5306
jackkrzyz <jackkrzyz@gmail.com>
2025-02-10 17:25:12 -0600

    add mongoose schemas for Period and Schedule models

---
+++ server/model/schedule.js
@@ -0,0 +1,22 @@
+/**
+ * schema for a schedule
+ */
+const mongoose = require("mongoose");
+const scheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  periods: {
+    type: [mongoose.Schema.ObjectId],
+    ref: "Period",
+    required: true,
+  },
+  enabled: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Schedule = mongoose.model("Schedule", scheme);
+module.exports = Schedule;
