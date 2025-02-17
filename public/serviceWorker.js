746c8c60cd405efc339e7213a7cb909b975c4480
Emran Kohl <115116866+Wrycabbage@users.noreply.github.com>
2025-02-16 23:55:05 -0600

    Everything mobile (#128)
    * first implementation for service worker to work with Iphone. Need to test to see if it shows as a regular app once added to home screen and then notificatons can be added next.
    
    Not sure if everything is ajusted correctly, the current starting page is auth.js so I started all the handling in that page.
    
    * Move serviceWorker code to client side. There used to be no file named auth2.js but has always been referenced as the script to auth.ejs
    
    * Add message to see if it is showing on the server
    
    * Testing to try to get app to apear like a normal app when added to home screen on mobile. Doesn't work
    
    * Super broken, need to try again
    
    * Moved to public file. The app now shows as a normal app when added to home screen.
    
    * Web app shows up as normal app now. Add to home screen page only shows up when on phone and not in real app
    
    * Change logo image picture. Still need to fix error of image not being shown because of it not accessible. Think the problem is that the service worker cannot get to it
    
    * Code to make sure only the newest service worker is running at one time
    
    * Only One service worker
    
    * Make side bar collapse leftward. Styling tests With Bennett Jesso
    
    * The App Looks Super Cool And Coffee Like Now
    
    * Finished Reworking the customize drink page
    
    * Fix Overlap Bug From animations
    
    * Restyle barista page to look super cool
    
    * Fully finish barista styling
    
    * Finish Cancel orders page and completed orders styling
    
    * Change remove button color in myCart
    
    * Fix side bar coloration
    
    * ReStyle Teacher Favorite Page
    
    * Restyle order history page
    
    * Restyle Add Drink Page
    
    * Restyle schedular part 1
    
    * Finsih Restylinh Schedyker
    
    * Finally get service worker to get app logo
    
    * New App Icon
    
    * Fix auth page header
    
    * add coffee design to bottom of screen
    
    * turn sidebar into hamburger on mobile
    
    * Fix Side scroll bar on sidebar
    
    * fix order extending past barista page
    
    * Make teacher email not include the part after the @ sign
    
    * Color background of header on mobile
    
    * Add client view button on adim header
    
    * center everything on mobile
    
    * Restyle mobile cart
    
    ---------
    
    Co-authored-by: Emran Kohl <wrycabbage@Emrans-MacBook-Pro.local>
    Co-authored-by: Jack Krzyzanowski <69120127+Jackkrzyz@users.noreply.github.com>

---
+++ public/serviceWorker.js
@@ -0,0 +1,52 @@
+const staticNNHSCoffe = "nnhs-coffee-site-v1";
+const assets = [
+  "/",
+  "/add-to-home.html",
+  "/manifest.json",
+  "/test-icon.png",
+  "/assets/js/auth2.js",
+  "/css/styles.css",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticNNHSCoffe).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Ensure the newest service worker activates immediately.
+});
+
+// New activate event to clean up old caches.
+self.addEventListener("activate", (activateEvent) => {
+  activateEvent.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== staticNNHSCoffe) {
+              return caches.delete(cacheName);
+            }
+            return Promise.resolve();
+          })
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  const requestUrl = new URL(fetchEvent.request.url);
+  if (!assets.includes(requestUrl.pathname)) {
+    fetchEvent.respondWith(fetch(fetchEvent.request));
+    return;
+  }
+  fetchEvent.respondWith(
+    caches
+      .match(fetchEvent.request)
+      .then((res) => res || fetch(fetchEvent.request))
+  );
+});
