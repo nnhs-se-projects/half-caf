bccdb75e006ce2d65ee2d6f32452038610c7d6b1
jackkrzyz <jackkrzyz@gmail.com>
2025-02-21 16:19:26 -0600

    Add reload button for delivery home
    change font color in metrics

--- assets/js/deliveryHome.js
+++ assets/js/deliveryHome.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const reloadButton = document.querySelector(".reload-button");
+  reloadButton.addEventListener("click", () => {
+    window.location.href = "/deliveryHome";
+  });
   const claimButtons = document.querySelectorAll("button.claimButton");
-
   for (const claimButton of claimButtons) {
     claimButton.addEventListener("click", async () => {
       const orderId = claimButton.value;
