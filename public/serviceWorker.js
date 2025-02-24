3393678f6673aa263c558b8890bd6381c4366a6d
Wrycabbage <egkohl279@gmail.com>
2025-02-24 08:54:50 -0600

    Remove unneeded notification code

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -65,10 +65,3 @@ self.addEventListener("message", (event) => {
     self.registration.showNotification(title, options);
   }
 });
-
-self.addEventListener("push", (event) => {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || "Notification";
-  const options = data.options || {};
-  event.waitUntil(self.registration.showNotification(title, options));
-});
