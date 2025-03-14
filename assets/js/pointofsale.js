22dc21e658fa593aa64fcfc75c1ae024e65807ce
Jackkrzyz <jackkrzyz@gmail.com>
2025-03-14 15:01:54 -0500

    continue work on the modifications

--- assets/js/pointofsale.js
+++ assets/js/pointofsale.js
@@ -1,8 +1,12 @@
-let currentDrinkId;
+let currentMenuItemId;
+let currentDrink;
+let cart = [];
 function addDrinkToOrder(drink) {
+  cart.push(drink);
   const orderTable = document.querySelector("#order-table");
   const drinkElement = document.createElement("tr");
   drinkElement.innerHTML = `
+    <input type="hidden" class="drink" value="${drink}">
     <td>${drink.name}</td>
     <td>$${drink.price}</td>
     <td>1</td>
@@ -12,15 +16,23 @@ function addDrinkToOrder(drink) {
     </td>
   `;
   orderTable.getElementsByTagName("tbody")[0].appendChild(drinkElement);
-  const cancelButton = drinkElement.querySelector(".cancelButton");
   const orderTotal = document.querySelector(".order-total");
+  orderTotal.textContent = Number(orderTotal.textContent) + Number(drink.price);
+  const cancelButton = drinkElement.querySelector(".cancelButton");
   cancelButton.addEventListener("click", async () => {
     orderTotal.textContent =
       Number(orderTotal.textContent) - Number(drink.price);
     drinkElement.remove();
+    cart.splice(cart.indexOf(drink), 1);
   });
-  currentDrinkId = drink.id;
-  orderTotal.textContent = Number(orderTotal.textContent) + Number(drink.price);
+  currentMenuItemId = drink.menuItemId;
+  currentDrink = drink;
+
+  drinkElement.addEventListener("click", async () => {
+    const clickedDrink = drinkElement.querySelector(".drink").value;
+    currentMenuItemId = clickedDrink.menuItemId;
+    currentDrink = clickedDrink;
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -29,11 +41,11 @@ document.addEventListener("DOMContentLoaded", () => {
     card.addEventListener("click", async () => {
       const drinkName = card.querySelector(".drink-name").value;
       const drinkPrice = card.querySelector(".drink-price").value;
-      const drinkId = card.querySelector(".drink-id").value;
+      const menuItemId = card.querySelector(".menuItem-id").value;
       const drink = {
         name: drinkName,
         price: drinkPrice,
-        id: drinkId,
+        menuItemId: menuItemId,
         flavors: [],
         toppings: [],
         temps: [],
